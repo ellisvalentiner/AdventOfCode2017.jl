924da01e90c54d167be0b4681aff18845f804ea3
Ellis Valentiner <ellisvalentiner@gmail.com>
2017-12-02 18:44:12 -0500

    Refactor

--- src/puzzles/puzzle02.jl
+++ src/puzzles/puzzle02.jl
@@ -1,16 +1,15 @@
 
 arrayrange(a, dims) = maximum(a, dims) - minimum(a, dims)
+removediag(m::Matrix) = (m[diagind(m)]=1e-05)
+getintegers(m::Matrix) = m[find(isinteger.(m))]
 
 function puzzle02(path::String=joinpath(@__DIR__, "..", "data/02.txt"))
     x = readdlm(path, Int)
     partone = arrayrange(x, 2) |> sum
 
-    parttwo = 0
-    for i in 1:size(x, 1)
-        y = x[i,:] ./ transpose(x[i,:])
-        y[diagind(y)] = -.1
-        parttwo += y[find(isinteger.(y))]
-    end
+    y = [x[i,:] ./ x[i,:]' for i in 1:size(x, 2)]
+    removediag.(y)
+    parttwo = Int(sum(getintegers.(y))[1])
 
-    return [partone, Int(parttwo[1])]
+    return [partone, parttwo]
 end
