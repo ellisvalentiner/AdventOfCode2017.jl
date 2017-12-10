10a4f6812cf18896af9086d7a32c88dedb7ec12e
Ellis Valentiner <ellisvalentiner@gmail.com>
2017-12-10 10:11:13 -0500

    Day 9

---
+++ src/puzzles/puzzle09.jl
@@ -0,0 +1,33 @@
+
+function puzzle09(path::String=joinpath(@__DIR__, "..", "data/09.txt"))
+    stream = readline(path)
+
+    idx = 1
+
+    total = 0
+    noncanceled = 0
+
+    arr = []
+    groups = 0
+    isgarbage = false
+
+    while idx <= length(stream)
+        if stream[idx] == '!'
+            idx += 1
+        elseif isgarbage
+            stream[idx] == '>' ? isgarbage = false : noncanceled += 1
+        elseif stream[idx] == '{'
+            groups += 1
+            append!(arr, cscore)
+        elseif stream[idx] == '<'
+            isgarbage = true
+        elseif stream[idx] == '}'
+            groups -= 1
+            total += pop!(arr)
+        end
+        idx += 1
+    end
+
+    return [total, noncanceled]
+
+end
