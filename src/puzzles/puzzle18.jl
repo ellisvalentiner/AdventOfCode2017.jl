8650fb83eeca7521375047a2c64545015eb2a4a9
Ellis Valentiner <ellisvalentiner@gmail.com>
2017-12-18 15:20:27 -0500

    WIP

--- src/puzzles/puzzle18.jl
+++ src/puzzles/puzzle18.jl
@@ -2,6 +2,14 @@
 snd(x::String) = append!(played, registers[x])
 set(x::String, y::String) = registers[x] = registers[y]
 set(x::String, y::Int) = registers[x] = y
+add(x::String, y::String) = registers[x] += registers[y]
+add(x::String, y::Int) = registers[x] += y
+mul(x::String, y::String) = registers[x] *= registers[y]
+mul(x::String, y::Int) = registers[x] *= y
+mod(x::String, y::String) = registers[x] = rem(registers[x], registers[y])
+mod(x::String, y::Int) = registers[x] = rem(registers[x], y)
+rcv(x::String, y::String) = (registers[x] != 0) ? registers[x] = last(played) : nothing
+rcv(x::String, y::Int) =
 
 function puzzle18(path::String=joinpath(@__DIR__, "..", "data/18.txt"))
 
@@ -22,31 +30,34 @@ function puzzle18(path::String=joinpath(@__DIR__, "..", "data/18.txt"))
 
         if command == "snd"
             append!(played, registers[x])
+            idx += 1
         elseif command == "set"
             y = parse(y)
             if typeof(y) == Symbol
                 y = registers[string(y)]
             end
             registers[x] = y
+            idx += 1
         elseif command == "add"
             y = parse(instruction[7:end])
             if typeof(y) == Symbol
                 y = registers[string(y)]
             end
             registers[x] += y
+            idx += 1
         elseif command == "mul"
             y = parse(instruction[7:end])
             if typeof(y) == Symbol
                 y = registers[string(y)]
             end
             registers[x] *= y
+            idx += 1
         elseif command == "mod"
             y = parse(instruction[7:end])
             if typeof(y) == Symbol
                 y = registers[string(y)]
             end
-            x = registers[x]
-            registers[x] = rem(x, y)
+            registers[x] = rem(registers[x], y)
             idx += 1
         elseif command == "rcv"
             if registers[x] != 0
@@ -66,7 +77,6 @@ function puzzle18(path::String=joinpath(@__DIR__, "..", "data/18.txt"))
                 idx += 1
             end
         end
-        idx += 1
     end
 
     return [last(played), nothing]
